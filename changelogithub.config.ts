663188c10ab4112572157f2fd19884c7b2106d90
cole <bernankez@qq.com>
2023-02-27 16:36:32 +0800

    ci: add workflow

---
+++ changelogithub.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "changelogithub";
+
+// ref: https://github.com/antfu/changelogithub/blob/main/src/config.ts
+export default defineConfig({
+  types: {
+    improve: { title: "💡 Improvement" },
+  },
+});
