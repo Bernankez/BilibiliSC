2f120da163decd6fc22f8b222b3619fca46f9a49
cole <bernankez@qq.com>
2023-02-28 15:28:53 +0800

    chore: fix ssg build

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import type { RouteRecordRaw } from "vue-router";
-import { createRouter, createWebHistory } from "vue-router";
 
 export const routes: RouteRecordRaw[] = [
   {
@@ -7,8 +6,3 @@ export const routes: RouteRecordRaw[] = [
     component: () => import("@/views/Home.vue"),
   },
 ];
-
-export const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
