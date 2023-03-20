3715582686d895de485316062a5823f84407a131
cole <bernankez@qq.com>
2023-03-20 17:45:18 +0800

    feat: add github link

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,6 +1,13 @@
 <template>
-  <header class="absolute h-18 inset-0 flex items-center p-x-3 box-border">
+  <header class="absolute h-18 inset-0 flex justify-between items-center p-x-6 box-border">
     <div></div>
+    <div class="flex flex-gap-6 items-center">
+      <!-- TODO -->
+      <div class="i-uil:comment-info text-6 cursor-pointer"></div>
+      <a href="https://github.com/Bernankez/BilibiliSC" target="_blank" title="GitHub">
+        <div class="i-uil:github-alt text-6 text-default cursor-pointer"></div>
+      </a>
+    </div>
   </header>
 </template>
 
@@ -9,4 +16,7 @@
 </script>
 
 <style scoped>
+a {
+  all: unset;
+}
 </style>
